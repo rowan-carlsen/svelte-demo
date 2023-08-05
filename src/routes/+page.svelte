fcc889c8ef8929fb25c6521eda218013f498fb7d
Rowan Carlsen <rowan.carlsen@gmail.com>
2023-08-05 18:01:22 -0400

    final demo additions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { crossfade } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
+	import Car from './Car.svelte';
 	const [send, receive] = crossfade({
 		duration: (d) => Math.sqrt(d * 200),
 		fallback() {
@@ -17,30 +18,32 @@
 	let name = 'World',
 		items = [
 			{
-				label: 'Do laundry',
+				task: 'Do laundry',
 				done: false
 			},
 			{
-				label: 'Brush teeth',
+				task: 'Brush teeth',
 				done: true
 			},
 			{
-				label: 'Plot world domination',
+				task: 'Plot world domination',
 				done: false
 			},
 			{
-				label: 'Clean floors',
+				task: 'Clean floors',
 				done: false
 			},
 			{
-				label: 'Buy groceries',
+				task: 'Buy groceries',
 				done: false
 			},
 			{
-				label: 'Call Grandma',
+				task: 'Call Grandma',
 				done: false
 			}
-		];
+		],
+		speed = 1,
+		showCar = false;
 	function check(item) {
 		item.done = !item.done;
 		items = [...items.filter((i) => i !== item), item];
@@ -50,23 +53,35 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<h2>"A" Detector: {name.includes('A') || name.includes('a') ? 'Found' : 'Not found'}</h2>
-	<input bind:value={name} />
+	<input type="text" bind:value={name} />
 	<div id="list-holder">
 		<ul class="list" id="todo-list">
-			{#each items.filter((i) => !i.done) as item (item.label)}
+			{#each items.filter((i) => !i.done) as item (item.task)}
 				<li in:receive={{ key: item.label }} out:send={{ key: item.label }}>
-					<button type="button" on:click={() => check(item)}> {item.label}</button>
+					<label>
+						<input type="checkbox" checked={item.done} on:change={check(item)} />
+						{item.task}
+					</label>
 				</li>
 			{/each}
 		</ul>
 		<ul class="list" id="completed-list">
-			{#each items.filter((i) => i.done) as item (item.label)}
+			{#each items.filter((i) => i.done) as item (item.task)}
 				<li in:receive={{ key: item.label }} out:send={{ key: item.label }}>
-					<button type="button" on:click={() => check(item)}> {item.label}</button>
+					<label>
+						<input type="checkbox" checked on:change={check(item)} />
+						{item.task}
+					</label>
 				</li>
 			{/each}
 		</ul>
 	</div>
+	{#if !showCar}
+		<button on:click={() => (showCar = !showCar)} type="button">Let's Drive!</button>
+	{:else}
+		<label>Speed:<input type="range" min="1" max="5" bind:value={speed} /></label>
+		<Car {speed} />
+	{/if}
 </main>
 
 <style>
@@ -74,7 +89,7 @@
 	h2 {
 		text-align: center;
 	}
-	input {
+	input[type='text'] {
 		width: 10em;
 		margin: 0 auto;
 		display: block;
@@ -94,15 +109,25 @@
 	li {
 		border: 1px solid white;
 	}
-	button {
-		font-family: inherit;
-		font-size: 1rem;
+	label {
 		padding: 0.5em;
-		background: none;
-		border: none;
-		color: inherit;
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
 		cursor: pointer;
-		width: 100%;
-		text-align-last: left;
+	}
+	input[type='checkbox'] {
+		width: 1rem;
+		height: 1rem;
+	}
+	button {
+		font-size: 1rem;
+		font-family: inherit;
+		background: teal;
+		cursor: pointer;
+		border: 2px solid white;
+		padding: 0.5em;
+		display: block;
+		margin: 2em auto 0 auto;
 	}
 </style>
---
+++ src/routes/Car.svelte
@@ -0,0 +1,59 @@
+<script>
+	export let speed = 1;
+</script>
+
+<div id="road" style="animation-duration: {0.5 / speed}s;">
+	<div id="car" />
+</div>
+
+<style>
+	#road {
+		background-color: black;
+		background-image: linear-gradient(to right, transparent 50%, black 50%),
+			linear-gradient(to bottom, black 48.5%, yellow 48.5%, yellow 51.5%, black 51.5%);
+		background-size: 100px 100%;
+		background-position: 0 0;
+		background-repeat: repeat-x;
+		position: relative;
+		margin: 0 auto;
+		width: 80vw;
+		height: 20vw;
+		animation: drive 0.5s linear infinite;
+	}
+	#car {
+		position: absolute;
+		height: 35%;
+		width: 20%;
+		background-color: red;
+		border-radius: 20px;
+		bottom: 5%;
+		left: 10%;
+		background-image: linear-gradient(
+				to bottom,
+				transparent 10%,
+				black 10%,
+				black 90%,
+				transparent 90%
+			),
+			conic-gradient(white 0deg, white 90deg, transparent 90deg),
+			conic-gradient(transparent 90deg, white 90deg, white 180deg, transparent 180deg);
+		background-repeat: no-repeat;
+		background-size: 30% 100%, 20% 30%, 20% 30%;
+		background-position: 82% 0, 100% 0, 100% 100%;
+		animation: steer 2s infinite ease-in-out;
+		transform: translateY(10%);
+	}
+	@keyframes drive {
+		to {
+			background-position: -100px 0;
+		}
+	}
+	@keyframes steer {
+		50% {
+			transform: translateY(-10%);
+		}
+		100% {
+			transform: translateY(10%);
+		}
+	}
+</style>
